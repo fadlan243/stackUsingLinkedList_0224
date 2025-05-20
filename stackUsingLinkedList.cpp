45f76de26b8e921ffcf55acb28fa53b6cdfa2773
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-20 10:14:20 +0700

    add push function for stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -26,4 +26,14 @@ public : //akses modifier
         top = NULL;
     }
 
+    int push (int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value:" << value << endl;
+        return value;
+    }
+
     
