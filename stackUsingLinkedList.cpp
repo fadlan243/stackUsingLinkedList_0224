6842e873ddfd6cdb605c485650b66745d8ff92a1
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-20 10:27:06 +0700

    void peek else

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -53,4 +53,16 @@ public : //akses modifier
         {
             cout << "List is Empty; " << endl; 
         }
-        
+        else
+        {
+            Node *current = top;
+            while (current != NULL);
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+   
