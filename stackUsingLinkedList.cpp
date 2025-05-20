5f667734acbf17a7677b647a871498a8141350b9
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-20 10:19:20 +0700

    add safe pop function to stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -36,4 +36,14 @@ public : //akses modifier
         return value;
     }
 
-    
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty" << endl;
+        }
+
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+    }
+
